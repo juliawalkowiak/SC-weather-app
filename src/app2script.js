6aed1624da1a7c67f1a462ca632e70da7066cb2d
Julia <walkowiak.jula@gmail.com>
2022-11-24 21:05:27 +0100

    API integrated
    integrated API + formatted days

--- src/app2script.js
+++ src/app2script.js
@@ -25,30 +25,39 @@ dateElement.innerHTML = `Current Time: ${hour}:${minutes}`;
 let fullDateElement = document.querySelector("#current-day");
 fullDateElement.innerHTML = ` ${days[dayIndex]}, ${dateIndex}<sup>th</sup> `;
 
+function formatDay (timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days =["Sun", "Mon", "Tue","Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
 
   console.log(response.data.daily);
+  let forecast = response.data.daily;
+
 
   let forecastElement = document.querySelector("#forecast");
   
   let forecastHTML =`<div class="row">`;
   
   let days = ["Mon","Tue", "Wed", "Thu", "Fri", "Sat"];
- 
 
-  days.forEach(function (day){ 
+  forecast.forEach(function (forecastDay){ 
   forecastHTML =  forecastHTML +  `
   
   <div class="col-2 card">
-     <div class="weather-forecast-date">${day}</div>
+     <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
         <img 
-          src="http://openweathermap.org/img/wn/50d@2x.png"
+          src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
            alt=""
            width="42"
           />
               <div class="weather-forecast-temperatures">
-              <span class="weather-forecast-temperature-max"> 18° </span>
-            <span class="weather-forecast-temperature-min"> 12° </span>
+              <span class="weather-forecast-temperature-max"> ${forecastDay.temp.max}° </span>
+            <span class="weather-forecast-temperature-min"> ${forecastDay.temp.min}° </span>
               </div>
         </div>
     
@@ -94,7 +103,7 @@ function displayWeatherCondition(response) {
         getForecast(response.data.coord);
       }
       
-      displayForecast();
+      
       
       function search(city) {
         console.log(city);
